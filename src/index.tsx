d32f10dd58a26c03a1a29686f8bdea4625d76f24
vkamunya <vanessawanjirundumia@gmail.com>
2022-04-21 18:01:06 +0300

    updates on button component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { Container } from "./styles"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export type ButtonProps = {
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void; // to handle onClick functions
+  children?: React.ReactNode; // make the component able to receive children elements
+  color?: 'primary' | 'secondary'; // two styling options (you can create as many as you want)
+  disabled?: boolean; // make the button disabled or not
+};
+
+export const Button = () => ({
+  onClick,
+  children,
+  color = "primary",
+  disabled,
+}: ButtonProps) => {
+  return(
+    <Container onClick={onClick} color={color} disabled={disabled}>
+    {children}
+    </Container>
+  );
+};
+
