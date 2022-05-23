99e8dfbda777e8e964e77a27ee0d7edc1f5abcdb
Qodestackr <winchygichu@gmail.com>
2022-05-23 06:20:48 +0300

    add: containers/Home and some containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ThankyouModal from './components/modals/ThankyouModal';
+import Home from './screens/Home/Home';
 function App() {
   return (
     <div className="flex justify-center items-center p-[10px]">
       <ThankyouModal/>
+      <Home />
     </div>
   );
 }
