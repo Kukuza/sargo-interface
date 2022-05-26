702e726fd6e24e614fb0f9218d46e2d7220d77c2
Mutinda-Kioko <kiokojohn2275@gmail.com>
2022-05-26 12:44:35 +0300

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Footer from './containers/Footer';
+
+
 function App() {
 
   return (
     <div className="flex justify-center items-center p-[10px]">
       <Footer/>
     </div>
+
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <BrowserRouter basename="/">
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
