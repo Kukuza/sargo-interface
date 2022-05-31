185121e449375755fd9976dba231b9e0be61b229
Qodestackr <winchygichu@gmail.com>
2022-05-31 14:44:45 +0300

    minor

--- src/components/menu/MobileMenu.tsx
+++ src/components/menu/MobileMenu.tsx
@@ -10,7 +10,7 @@ function MobileMenu() {
   
   return (
 
-    <div className='flex flex-col gap-3 hover:cursor-pointer absolute md:hidden right-2 mr-3' 
+    <div className='flex flex-col gap-1 hover:cursor-pointer absolute md:hidden right-2 mr-3' 
          onClick={handleMenuToggle}>
       {!openMenu ?
       (<><span className='h-[2px] w-[30px] bg-[#111111]'></span>
