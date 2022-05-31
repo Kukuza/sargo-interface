a0bb89306b4b0b3a641e06f25e7c99de28951b18
Qodestackr <winchygichu@gmail.com>
2022-05-31 14:54:06 +0300

    resolving merge-conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import ThankyouModal from './components/modals/ThankyouModal';
 import NavHeader from './containers/NavHeader';
 function App() {
   return (
     <div className="flex justify-center items-center p-[10px]">
       <NavHeader />
-      <ThankyouModal/>
     </div>
   );
 }
